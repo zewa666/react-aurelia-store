27273f40085db477adefa2b439fbdbd78cf7ffc8
Vildan Softic <zewa666@gmail.com>
2019-12-08 19:33:27 +0100

    feat(store): added sample of using immerjs

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,7 +1,10 @@
+import produce from "immer";
 import { State, store } from "./state";
 
 export function addFramework(state: State, newFramework: string) {
-  return Object.assign({}, state, { frameworks: [ ...state.frameworks, newFramework ] } );
+  return produce(state, draftState => {
+    draftState.frameworks.push(newFramework);
+  });
 }
 
 store.registerAction("addFramework", addFramework);
